f2d1b472db42e97e763a49ab7eefe67e557cf8f1
DawidMk <dawid_m@interia.pl>
2019-08-24 09:25:30 +0200

    Initial commit

---
+++ src/main/java/pl/sda/meetup2/Meetup2Application.java
@@ -0,0 +1,13 @@
+package pl.sda.meetup2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Meetup2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Meetup2Application.class, args);
+    }
+
+}
